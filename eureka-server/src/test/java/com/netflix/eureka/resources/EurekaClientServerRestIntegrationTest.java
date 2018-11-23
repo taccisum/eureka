e01055fb6bfe174e2fe052b2cc554dcd0b4183b0
taccisum <514162920@qq.com>
2018-11-23 09:43:02 +0800

    add code for debug

--- eureka-server/src/test/java/com/netflix/eureka/resources/EurekaClientServerRestIntegrationTest.java
+++ eureka-server/src/test/java/com/netflix/eureka/resources/EurekaClientServerRestIntegrationTest.java
@@ -232,6 +232,17 @@ public class EurekaClientServerRestIntegrationTest {
     }
 
     private static void startServer() throws Exception {
+//        server = new Server(8080);
+//
+//        // TODO Thread.currentThread().getContextClassLoader() 获取不到路径，先暂时这样；
+//        WebAppContext webAppCtx = new WebAppContext(new File("./eureka-server/src/main/webapp").getAbsolutePath(), "/");
+//        webAppCtx.setDescriptor(new File("./eureka-server/src/main/webapp/WEB-INF/web.xml").getAbsolutePath());
+//        webAppCtx.setResourceBase(new File("./eureka-server/src/main/resources").getAbsolutePath());
+//        webAppCtx.setClassLoader(Thread.currentThread().getContextClassLoader());
+//        server.setHandler(webAppCtx);
+//        server.start();
+//
+//        eurekaServiceUrl = "http://localhost:8080/v2";
         File warFile = findWar();
 
         server = new Server(8080);
